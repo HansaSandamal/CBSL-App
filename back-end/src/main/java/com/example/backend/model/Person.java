cd8bbc10ff4e783386e1e29ac666dafd8841f6f1
HansaSandamal <hansasandamal@gmail.com>
2021-09-09 21:21:15 +0530

    add db

--- back-end/src/main/java/com/example/backend/model/Person.java
+++ back-end/src/main/java/com/example/backend/model/Person.java
@@ -1,4 +1,4 @@
-package com.hansa.backend.model;
+package com.example.backend.model;
 
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
