eb08a9d174c319b55f5f0b026d59262629124062
HansaSandamal <hansasandamal@gmail.com>
2021-09-10 13:51:08 +0530

    db connection

--- back-end/src/main/java/com/example/backend/controller/PersonController.java
+++ back-end/src/main/java/com/example/backend/controller/PersonController.java
@@ -1,11 +1,16 @@
 package com.example.backend.controller;
 
 
+import com.example.backend.model.Person;
 import com.example.backend.repository.PersonRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
 
 @CrossOrigin(origins = "http://localhost:3000")
 @RestController
@@ -13,6 +18,49 @@ import org.springframework.web.bind.annotation.RestController;
 public class PersonController {
     @Autowired
     PersonRepository personRepository;
+
+    @GetMapping("/persons")
+    public ResponseEntity<List<Person>> getAllPersons(@RequestParam(required = false) String firstName) {
+        try {
+            List<Person> persons = new ArrayList<Person>();
+
+            if (firstName == null)
+               personRepository.findAll().forEach(persons::add);
+            else
+                personRepository.findByFirstNameContaining(firstName).forEach(persons::add);
+
+            if (persons.isEmpty()) {
+                return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+            }
+
+            return new ResponseEntity<>(persons, HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @GetMapping("/persons/{nic}")
+    public ResponseEntity<Person> getPersonByNic(@PathVariable("nic") String nic) {
+        Optional<Person> personData = personRepository.findByNicContaining(nic);
+
+        if (personData.isPresent()) {
+            return new ResponseEntity<>(personData.get(), HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+//    @GetMapping("/persons/{id}")
+//    public ResponseEntity<Person> getPersonById(@PathVariable("id") String id) {
+//        Optional<Person> personData2 = personRepository.findById(id);
+//
+//        if (personData2.isPresent()) {
+//            return new ResponseEntity<>(personData2.get(), HttpStatus.OK);
+//        } else {
+//            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+//        }
+//    }
+
+
 
 
 }
--- back-end/src/main/java/com/example/backend/repository/PersonRepository.java
+++ back-end/src/main/java/com/example/backend/repository/PersonRepository.java
@@ -4,8 +4,10 @@ import com.example.backend.model.Person;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface PersonRepository extends MongoRepository<Person,String> {
-    List<Person>findByNicContaining(String nic);
+    Optional<Person> findByNicContaining(String nic);
+    List<Person> findByFirstNameContaining(String firstName);
 
 }
